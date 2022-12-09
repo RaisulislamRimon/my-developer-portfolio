f2118db88e7263f756dad613e2df53c6b42f8a73
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-10 01:04:56 +0600

    banner effect bg-color update

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -6,13 +6,13 @@ const Banner = () => {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
+            <h1 className="text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold lg:ml-2">
               MD RAISUL ISLAM RIMON
             </h1>
-            <h2 className="py-6 text-sm sm:text-base md:text-xl font-bold">
+            <h2 className="py-6 text-sm sm:text-base md:text-xl  lg:ml-2 font-bold">
               FRONT END WEB DEVELOPER
             </h2>
-            <div className="flex gap-3 mt-5">
+            <div className="flex gap-3 mt-5  lg:ml-2">
               <div>
                 <a className="btn rounded-3xl px-8">Download Resume</a>
               </div>
@@ -21,13 +21,13 @@ const Banner = () => {
               </div>
             </div>
           </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card flex-shrink-0 w-full max-w-sm hover:shadow-2xl hover:shadow-current hover:cursor-crosshair">
             <div className="card-body">
               <div className="avatar">
                 <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                   <img
                     src="https://i.ibb.co/6n23vsp/rimon-photopea-edit-removebg-preview-2.png"
-                    className="max-w-sm rounded-lg shadow-2xl"
+                    className="max-w-sm rounded-lg shadow-current"
                   />
                 </div>
               </div>
