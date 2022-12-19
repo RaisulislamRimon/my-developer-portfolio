b2fe9f42d18957f1c129c913039c6be42247d736
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-19 12:05:01 +0600

    update blog page

--- src/components/Home/Blog/Blog.jsx
+++ src/components/Home/Blog/Blog.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const Blog = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center mt-20 mb-32">Blog</h1>
-      <div className="min-h-screen text-2xl text-center">Comming Soon...</div>
+      <h1 className="text-4xl text-center mt-20 mb-32 text-white underline">
+        Blog
+      </h1>
+      <div className="min-h-screen text-2xl text-center text-white">
+        Comming Soon...
+      </div>
     </div>
   );
 };
