d35101b9950fc96d2dd1bac6153ac369bb5ca274
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-19 11:57:21 +0600

    loading & loading error added

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -1,13 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
+import Loading from "../../Shared/Loading/Loading";
+import LoadingError from "../../Shared/Loading/LoadingError";
 import Project from "../Project/Project";
 
 const Projects = () => {
-  const [projects, setProjects] = useState([]);
-  useEffect(() => {
-    fetch("https://my-developer-portfolio-server.vercel.app/projects")
-      .then((res) => res.json())
-      .then((data) => setProjects(data));
-  }, []);
+  // const [projects, setProjects] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://my-developer-portfolio-server.vercel.app/projects")
+  //     .then((res) => res.json())
+  //     .then((data) => setProjects(data));
+  // }, []);
+  const {
+    isLoading,
+    error,
+    data: projects = [],
+  } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch("https://my-developer-portfolio-server.vercel.app/projects").then(
+        (res) => res.json()
+      ),
+  });
+
+  if (isLoading) return <Loading />;
+
+  // if (error) return "An error has occurred: " + error.message;
+  if (error) return <LoadingError />;
   return (
     <div className="min-h-screen my-20">
       <h1 className="text-2xl md:text-3xl lg:text-4xl text-center text-white my-10">
